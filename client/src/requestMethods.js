9fa9cc3678c72280d6c53736a5ad9b764aba4c36
Makarand Nagvekar <makarandnagvekar1036@gmail.com>
2025-02-12 19:46:02 +0530

    deployment issues resolved

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}`;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
